333ac6280bf22261444972899f25c64fa34b55aa
Abhiuday <gupta.abhiuday.109@gmail.com>
2026-06-10 10:53:37 +0530

    refactor: remove @EnableScheduling, switch Dockerfile to Maven image

--- notification-service/src/main/java/com/mangacdc/NotificationApplication.java
+++ notification-service/src/main/java/com/mangacdc/NotificationApplication.java
@@ -2,10 +2,7 @@ package com.mangacdc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
 @SpringBootApplication
-@EnableScheduling
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
